94916adbde91c23efabb72a5b7c54aa1ebccc32a
root <root@localhost.localdomain>
2019-06-30 21:33:44 +0200

    Validate JavaScript V2

--- signup.php
+++ signup.php
@@ -1,18 +1,23 @@
 <?php require "header.php" ?>
   <main>
     <scetion class="section-default">
-      <p id='error'></p>
-        <form id="formone" name="signform" class="signup-form" action="includes/signup.inc.php" method="post" onsubmit="return secureform()">
+        <form id="formone" onsubmit="return crousa();" name="signform" class="signup-form" action="includes/signup.inc.php" method="post" >
           <input type="text" name="unm" placeholder="Username" minlength="6" maxlength="20" required />
+          <span id="errorUser"></span>
           <br/>
-          <input type="email" name="mail" placeholder="E-mail" minlength="6" maxlength="20" required/>
+          <input type="email" name="mail" placeholder="E-mail" minlength="15" maxlength="42" required/>
+          <span id="errorEmail"></span>
           <br/>
-          <input type="password" name="pwdr" placeholder="Password" minlength="6" maxlength="20" required/>
+          <input type="password" name="pwdr" placeholder="Password" minlength="12" maxlength="22" required/>
+          <span id="errorPass"></span>
           <br/>
-          <input type="password" name="pwdrc" placeholder="Confirm Password" minlength="6" maxlength="20" required/>
+          <input type="password" name="pwdrc" placeholder="Confirm Password" minlength="12" maxlength="22" required/>
+          <span id="errorPassC"></span>
           <br/>
-          <button type="submit" name="signup-submit">SignUp</button>
+          <button type="submit" name="signupsubmit">SignUp</button>
+          <p id="submitError"></p>
         </form>
+
     </scetion>
   </main>
 <?php require "footer.php" ?>
--- style/main.js
+++ style/main.js
@@ -2,27 +2,52 @@ var aboutme = document.getElementById('about');
 aboutme.onclick = function aboutus(){
   alert('Designed By Mahmoud');
 };
-//Validate The Input (FrontEnd)
+//Validate  HYDRA VALIDATE The Input (FrontEnd)
 /*
 1-Inputs require
 2-input > 5 & password > 8 & Email > 8
 3-Not Empty
 4- Input !< 11 & Password !< 22 & email !< 20
 */
-var form = document.getElementById('formone');
-var allFormInput = form.elements;
-allFormInput[0].addEventListener("input",secureform)
-function secureform(){
-if(this.value == "" || this.value.indexOf("<") !== -1 || this.value.indexOf(">") !== -1 ||typeof(this.value) == null || typeof(this.value) == 'undefined'  || this.value.length <= 5 || this.value.length >= 20){
-document.getElementById('error').innerHTML = '<h1>Error Username </h1>';
-return false;
+var form          = document.getElementById('formone');
+var allFormInput  = form.elements;
+var dontCreat     = 0;
+var userName      = allFormInput.unm,
+    email         = allFormInput.mail,
+    passwordOne   = allFormInput.pwdr,
+    passwordCon   = allFormInput.pwdrc;
+    function valiform(data,errmessage,corrmessage,messageArea,slength,hlength){
+      if(data.value == "" || data.value.indexOf("<") !== -1 || data.value.indexOf(">") !== -1 || typeof(data.value) == null || typeof(data.value) == 'undefined'){
+        document.getElementById(messageArea).innerHTML = errmessage;
+        dontCreat = 1;
+        return false;
+      }
+      else if(data.value.search(/\s/g) > -1 || data.value.length <= slength || data.value.length >= hlength ){
+        document.getElementById(messageArea).innerHTML = errmessage;
+        dontCreat = 1;
+        return false;
+      }
+      else {
+        document.getElementById(messageArea).innerHTML = corrmessage;
+        dontCreat = 2;
+        return true;
+      }
+    }
+function crousa(){
+  if(dontCreat !== 2){
+    document.getElementById('submitError').innerHTML = 'Dont Joking With Me Back And Correct Your Error!!';
+    return false;
+  } else if (passwordOne.value !== passwordCon.value){
+    document.getElementById('errorPassC').innerHTML = 'Password Not Matched !';
+    dontCreat = 1;
+    return false;
+  }
+  else{
+    return true;
+  }
 }
-else if(this.value.search(/\s/g) > -1)
-{
-  document.getElementById('error').innerHTML = '<h1>Error whitespace </h1>';
-return false;
-}
-else {
-document.getElementById('error').innerHTML = '<h1 style=\"color:green\;\">Correct Username </h1>';
-}
-}
+/*  END OF THE HYDRA-VALIDATE FUNCTION */
+userName.addEventListener('input',function() { valiform(userName,'Not valid or correct username','Valid Keep going','errorUser',6,20); });
+email.addEventListener('input',function() { valiform(email,'Not valid email','Valid email','errorEmail',15,42); });
+passwordOne.addEventListener('input',function() { valiform(passwordOne,'Not valid Or Not Strong','Strong Password','errorPass',12,22); });
+passwordCon.addEventListener('input',function() { valiform(passwordCon,'Not Match Or Wrong Type Password','Strong Password','errorPassC',12,22); });
